7c507ba1fcdc501efa717f49b505568973615137
Anton_Gerasimov <antonsergeevichgerasimov@gmail.com>
2023-08-30 17:23:09 +0400

    Add sql scripts

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,20 +3,24 @@
 INSERT INTO ROLES VALUES
     (1, 'admin'),
     (2, 'manager'),
-    (3, 'common');
+    (3, 'common')
+    ON DUPLICATE KEY UPDATE ROLE_ID = ROLE_ID;
 
-INSERT INTO USERS VALUES
-    (1, 'Jimmy', 'Page', 'admin', 'admin', 1, TRUE),
-    (2, 'John', 'Doe', 'john', 'john', 2, TRUE);
+INSERT IGNORE INTO USERS (FIRST_NAME, LAST_NAME, EMAIL, USERNAME, PASSWORD, ROLE_ID, IS_ACTIVE) VALUES
+    ('John', 'Doe', '2@gmail.com',  'john', 'john', 2, TRUE),
+    ('Jimmy', 'Page', '1@gmail.com', 'admin', 'admin', 1, TRUE)
+
+    ON DUPLICATE KEY UPDATE USER_ID = USER_ID;
 
 INSERT INTO ADDRESSES VALUES
     (1, 2, 'elm', '12', '23'),
-    (2, 1, 'pine', '43', '1');
+    (2, 1, 'pine', '43', '1')
+    ON DUPLICATE KEY UPDATE ADDRESS_ID = ADDRESS_ID;
 
 INSERT INTO ORDERS VALUES
-    (1, 2, TO_DATE('17-11-2020', 'DD-MM-YYYY'), 'active', 2),
-    (2, 1, TO_DATE('7-9-2022', 'DD-MM-YYYY'), 'active', 1);
-
+    (1, 2, '2023-08-28 14:30:00', 'active', 2),
+    (2, 1, '2023-08-28 14:30:00', 'active', 1)
+    ON DUPLICATE KEY UPDATE ORDER_ID = ORDER_ID;
 
 
 COMMIT;
