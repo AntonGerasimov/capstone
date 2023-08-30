f0b13b2804acec3da0c76a00105931157312eaab
Anton_Gerasimov <antonsergeevichgerasimov@gmail.com>
2023-08-30 21:04:33 +0400

    Change role entity classes according to changes in scripts

--- src/main/java/com/gerasimov/capstone/dbclasses/entity/RoleEntity.java
+++ src/main/java/com/gerasimov/capstone/dbclasses/entity/RoleEntity.java
@@ -1,32 +1,18 @@
 package com.gerasimov.capstone.dbclasses.entity;
 
-import javax.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "ROLES")
+@Table(name = "roles")
+@Getter
+@Setter
 public class RoleEntity {
     @Id
-    @Column(name = "ROLE_ID")
     private Long id;
-    @Column(name = "ROLE_NAME")
     private String roleName;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getRoleName() {
-        return roleName;
-    }
-
-    public void setRoleName(String roleName) {
-        this.roleName = roleName;
-    }
 }
