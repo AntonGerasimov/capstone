0aa8c60557009b6b20e443911531888f675fc3dc
Anton_Gerasimov <antonsergeevichgerasimov@gmail.com>
2023-09-08 19:01:07 +0400

    Change all user passwords to '1111'

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,17 +7,17 @@ INSERT IGNORE INTO roles (name) VALUES
 
 INSERT IGNORE INTO users (first_name, last_name, email, username, password, role_id, is_active) VALUES
     ('admin', 'admin', 'admin@gmail.com', 'admin',
-    '$2a$12$elyM3bgM0Sdw877MIFDqxe7EuNUwvpXH14NDHwddStk6pdImuEGpu', 3, TRUE),
+    '$2a$12$WocppmSlhS5v.r8CN4SSUeJuCdwh6ZhQMOANEn.jh/knDCn4xy.Bi', 3, TRUE),
     ('John', 'Doe', 'john@gmail.com',  'john',
-    '$2a$12$jHbTTshgK7B4Zfg6aLv5x.ZgI709grtzor3hugeXt6WaZ6t3DK3Ie', 2, TRUE),
+    '$2a$12$WocppmSlhS5v.r8CN4SSUeJuCdwh6ZhQMOANEn.jh/knDCn4xy.Bi', 2, TRUE),
     ('Jimmy', 'Page', 'jimmy@gmail.com', 'jimmy',
-    '$2a$12$B8OMbDf0ju/P2zLJH9UjWeHvo7ldX9Zv8pqJ.UsmVKw1c/bM964Gq', 3, TRUE),
+    '$2a$12$WocppmSlhS5v.r8CN4SSUeJuCdwh6ZhQMOANEn.jh/knDCn4xy.Bi', 3, TRUE),
     ('Vasya', 'Pupkin', 'pupkin@gmail.com', 'vasya',
-    '$2a$12$Xq52Vb/0UXvC31AMqS2VGuksM.e2o8.BAt3EBOvWtn10qX9jVDZPK', 1, TRUE),
+    '$2a$12$WocppmSlhS5v.r8CN4SSUeJuCdwh6ZhQMOANEn.jh/knDCn4xy.Bi', 1, TRUE),
     ('John', 'Bonham', 'bonham@gmail.com', 'bonham',
-    '$2a$12$vb1YKkSe5UiS2urR4xPHPuOil6J9nC7AdQxyPRo2AnHakPGOj2J/e', 1, TRUE),
+    '$2a$12$WocppmSlhS5v.r8CN4SSUeJuCdwh6ZhQMOANEn.jh/knDCn4xy.Bi', 1, TRUE),
     ('Robert', 'Plant', 'plant@gmail.com', 'plant',
-    '$2a$12$emkMqAfaVlv1dJksZmeuEOCCTTVBh8o3LQNAcefBdTfUr1lv7VtI2', 1, TRUE);
+    '$2a$12$WocppmSlhS5v.r8CN4SSUeJuCdwh6ZhQMOANEn.jh/knDCn4xy.Bi', 1, TRUE);
 
 INSERT INTO addresses (user_id, street, house, apartment) VALUES
     (2, 'elm', '12', '23'),
