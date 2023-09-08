a11f49546ed8b6859bb1665d0fe53e6459f41df1
Anton_Gerasimov <antonsergeevichgerasimov@gmail.com>
2023-09-08 18:59:06 +0400

    Change method name

--- src/main/java/com/gerasimov/capstone/dbclasses/controllers/RootController.java
+++ src/main/java/com/gerasimov/capstone/dbclasses/controllers/RootController.java
@@ -16,7 +16,7 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 public class RootController {
 
     @GetMapping("/")
-    public String showIndex() {
+    public String showMainPage() {
         return "index";
     }
 
